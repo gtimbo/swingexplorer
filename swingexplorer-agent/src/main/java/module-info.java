e920a76f2b7dfb54940d53fc7b8d2c5f6ede6c80
Tim Wundke <gtwundke@gmail.com>
2021-04-25 07:44:55 +0930

    Added module-info.java files so that Swing Explorer can be used with the Java 9 module system (JPMS). Updated the POM to compile as Java 9, and updated plugins to latest versions.

---
+++ swingexplorer-agent/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module org.swingexplorer.instrument {
+    requires java.desktop;
+    requires java.instrument;
+    requires javassist;
+
+    exports org.swingexplorer.instrument;
+}
---
+++ swingexplorer-core/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.swingexplorer {
+    requires java.desktop;
+    requires java.management;
+    requires java.rmi;
+    requires org.swingexplorer.instrument;
+    requires swing.layout;
+
+    exports org.swingexplorer;
+}
